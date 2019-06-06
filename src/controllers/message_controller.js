48f63f1004152f89ec59c5f1ba041a133d05e0a5
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2019-06-06 19:31:26 +0200

    anotha one

--- src/controllers/message_controller.js
+++ src/controllers/message_controller.js
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 const Message = require('../models/message')
 const StreamMdl = require('../models/stream')
 
 
 function getAll(req, res) {
   Message.find({}, {_v: 0})
-=======
-const Message = require('../models/message');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-function getAll(req, res) {
-  Message.find({})
-  .sort('-createdAt')
->>>>>>> 9be2d3587a76454d90b3d80c2848dcebbb0b0605
     .then(messages => {
       res.status(200).send(messages);
     })
@@ -23,7 +12,6 @@ function getAll(req, res) {
     });
 }
 
-<<<<<<< HEAD
 function getStreamMessages(req,res) {
   StreamMdl.findById(req.params.id)
   .sort('-createdAt')
@@ -58,24 +46,10 @@ function create(req, res) {
       res.status(401).send({Error: 'Error while creating Message', err: err})
     })
   })
-=======
-function create(req, res) {
-  const newMessage = new Message(req.body);
-  newMessage.save(err => {
-    if (err) return res.status(500).send(err);
-    else {
-      io.emit('message', req.body);
-      return res.status(200).send(newMessage);
-   }
-   });
->>>>>>> 9be2d3587a76454d90b3d80c2848dcebbb0b0605
 };
 
 module.exports = {
   getAll,
-<<<<<<< HEAD
   getStreamMessages,
-=======
->>>>>>> 9be2d3587a76454d90b3d80c2848dcebbb0b0605
   create
 }
