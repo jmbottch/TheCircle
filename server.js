dd3a53c33c8a6eabda6ed0ad2cea92ccc33ec591
Sjoerd van der Bok <sabok@avans.nl>
2019-06-07 14:25:42 +0200

    socket stuff

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const cors = require('cors');
 var mongodb = require('./config/mongodb_connections');
 const app = express();
@@ -9,7 +9,7 @@ var io = require('socket.io')(http);
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(__dirname));
 
@@ -25,65 +25,63 @@ userroutes(app);
 messageroutes(app);
 streamroutes(app);
 
-mongodb.createDevConnection();  
+mongodb.createDevConnection();
 
 var messages = [];
 
 function emitNewMsg(userId) {
-  User.findById(userId, { __v: 0})
-  .populate('messages')
-  .then(foundUser => {
-    messages = foundUser.messages
-    console.log(this.messages)
-    io.sockets.emit('messages', messages);
-  });
+  User.findById(userId, { __v: 0 })
+    .populate('messages')
+    .then(foundUser => {
+      console.log('emitting:')
+      io.sockets.emit('messages', foundUser.messages);
+    });
 }
 
 app.post('/api/message/', function (req, res) {
   Message.create(req.body)
-  .then(msg => {
-    User.findById(req.body.host)
-    .then(foundUser => {
-      foundUser.messages.push(msg)
-      foundUser.save()
-      .then(() => {
-        emitNewMsg(req.body.host)
-        res.status(200).send({Message : 'Message saved'})
-      })
-      .catch(err => {
-        Message.remove(msg)
-        console.log(err);
-        res.status(401).send({Error:'Error while pushing Message to Host'})
-      })
+    .then(msg => {
+      User.findById(req.body.host)
+        .then(foundUser => {
+          foundUser.messages.push(msg)
+          foundUser.save()
+            .then(() => {
+              emitNewMsg(req.body.host)
+              res.status(200).send({ Message: 'Message saved' })
+            })
+            .catch(err => {
+              Message.remove(msg)
+              console.log(err);
+              res.status(401).send({ Error: 'Error while pushing Message to Host' })
+            })
+        })
+        .catch(err => {
+          Message.remove(msg)
+          res.status(401).send({ Error: 'Host not found' })
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(401).send({ Error: 'Error while creating Message' })
+        })
     })
-    .catch(err => {
-      Message.remove(msg)
-      res.status(401).send({Error:'Host not found'})
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(401).send({Error: 'Error while creating Message'})
-    })
-  })
 })
 
 io.on('connection', socket => {
   socket.on('getMsgs', userId => {
-    User.findById(userId, { __v: 0})
-  .populate('messages')
-  .then(foundUser => {
-    messages = foundUser.messages
-    console.log(this.messages)
-    socket.emit('getMsgs', messages);
-  });
+    User.findById(userId, { __v: 0 })
+      .populate('messages')
+      .then(foundUser => {
+        console.log('emitting:')
+        socket.emit('getMsgs', foundUser.messages);
+      });
   });
 
-//   socket.on('addMsg', msg => {
-//     io.emit('messages', messages);
-//     socket.emit('messages', messages);
-// });
-  
-  io.sockets.emit('messages', messages);
+  //   socket.on('addMsg', msg => {
+  //     io.emit('messages', messages);
+  //     socket.emit('messages', messages);
+  // });
+
+  //io.sockets.emit('messages', messages);
   console.log(`Socket ${socket.id} has connected`);
 });
 
@@ -91,5 +89,5 @@ http.listen(process.env.PORT | 5000, () => {
   console.log('server is running on port 5000');
 });
 
-module.exports = app ;
+module.exports = app;
 
