f28ca3b8b5fc7b60ac32b7ad2c30baa374cd9780
Sjoerd van der Bok <sabok@avans.nl>
2019-06-11 10:53:04 +0200

    Added profilepic to user

--- src/models/user.js
+++ src/models/user.js
@@ -19,6 +19,7 @@ const UserSchema = new Schema({
         },
         required: [true, 'Password is required.']
     },
+    profilePicture: String,
     messages: [{
         type: Schema.Types.ObjectId,
         ref: 'message',
