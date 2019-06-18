eedb515bc9273bb7d33ea4ec2e1a033b611f526e
Joep Gruter <jgj.gruter@student.avans.nl>
2019-06-18 16:06:34 +0200

    Updated config to enable stream fetching updated gitignore to prevent media uploading

--- config/nms_config.js
+++ config/nms_config.js
@@ -1,3 +1,6 @@
+const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+console.log(ffmpeg.path, ffmpeg.version);
+
 const config = {
     rtmp: {
         port: 1935,
@@ -8,8 +11,21 @@ const config = {
     },
     http: {
         port: 8000,
+        mediaroot: './media',
         allow_origin: '*'
-    }
+      },
+      trans: {
+        ffmpeg: ffmpeg.path,
+        tasks: [
+          {
+            app: 'live',
+            hls: true,
+            hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+            dash: true,
+            dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+          }
+        ]
+      }
 };
 
 module.exports = {
