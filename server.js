f7189af5ce7b7090dddf820f2fe465ae9c2695a8
Sjoerd van der Bok <sabok@avans.nl>
2019-06-19 11:23:45 +0200

    stop watching activity

--- server.js
+++ server.js
@@ -122,6 +122,7 @@ io.on('connection', socket => {
   });
 
   socket.on('stopWatching', userIds => {
+    ActivityController.addActivity(userIds.viewer, 'Stopped watching stream with host: ' + userIds.host, 'Stopped watching')
     removeFromOne(userIds.host, socket.id)
     io.emit('viewSingle', 'new viewcount');
   });
